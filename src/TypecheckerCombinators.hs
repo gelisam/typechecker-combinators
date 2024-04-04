1ccc56a9f61f4d0b2fec41d00d34234357d72360
Samuel Gélineau <gelisam+github@gmail.com>
2024-04-04 09:07:59 -0400

    write down the public API more explicitly

--- src/TypecheckerCombinators.hs
+++ src/TypecheckerCombinators.hs
@@ -1,15 +1,30 @@
 {-# LANGUAGE ExplicitNamespaces #-}
 module TypecheckerCombinators
-  ( MaybeT
-  , runMaybeT
-  , module X
-  , type (+)
-  , Elem
+  ( module X
   ) where
 
-import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Maybe as X
+  ( MaybeT(runMaybeT)
+  )
 
-import TypecheckerCombinators.Elem
+import TypecheckerCombinators.Elem as X
+  ( type (+)
+  , Elem
+  )
 import TypecheckerCombinators.Fix as X
+  ( Roll
+  , Fix(Fix, unFix)
+  , roll
+  , unroll
+  )
 import TypecheckerCombinators.MonadEq as X
+  ( MonadEq(assertEq)
+  )
 import TypecheckerCombinators.Typechecker as X
+    ( Handle(check, infer)
+    , TypeChecker
+    , runTypeChecker
+    , (<+>)
+    , checked
+    , infered
+    )
